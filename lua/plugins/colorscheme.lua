f3169bee00bbe1a91b89e9d411b7f02ef7898cb7
afyqeyzar <norafyqe@gmail.com>
2025-06-25 00:59:05 +0800

    feat: make bg slightly darker

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,19 +2,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = function()
-        require("nightfox").setup({
-          options = {
-            colorblind = {
-              enable = true,
-              severity = {
-                protan = 0.3,
-                deutan = 0.6,
-              },
-            },
-          },
-        })
-      end,
+      colorscheme = "carbonfox",
     },
   },
 }
--- lua/plugins/nightfox.lua
+++ lua/plugins/nightfox.lua
@@ -1,1 +1,30 @@
-return { "EdenEast/nightfox.nvim" }
+return {
+  "EdenEast/nightfox.nvim",
+  config = function()
+    local palettes = {
+      carbonfox = {
+        bg1 = "#151515",
+      },
+    }
+    require("nightfox").setup({
+      palettes = palettes,
+      options = {
+        styles = {
+          comments = "italic",
+          keywords = "bold",
+          functions = "italic,bold",
+        },
+        colorblind = {
+          enable = true,
+          severity = {
+            protan = 0.4,
+            deutan = 0.9,
+            tritan = 0.2,
+          },
+        },
+      },
+    })
+
+    vim.cmd("colorscheme carbonfox")
+  end,
+}
