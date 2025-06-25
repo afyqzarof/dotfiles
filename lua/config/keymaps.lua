20a38ba9ece0ca64a264a1fb53439dd49b620e22
afyqeyzar <norafyqe@gmail.com>
2025-06-26 00:10:35 +0800

    feat: preview image in arc

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>vi", function()
+  local file = vim.fn.expand("%:p")
+  local ext = file:match("^.+(%..+)$")
+
+  if ext == ".png" or ext == ".jpg" or ext == ".jpeg" or ext == ".gif" then
+    vim.cmd("silent !open -a 'Arc' " .. vim.fn.shellescape(file))
+  elseif ext == ".svg" then
+    -- Open SVG specifically in Arc Browser
+    vim.cmd("silent !open -a 'Arc' " .. vim.fn.shellescape(file))
+  else
+    vim.notify("Not a supported image file (png, jpg, svg, etc)", vim.log.levels.WARN)
+  end
+end, { desc = "View Image (opens Arc for SVGs)" })
