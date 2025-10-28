f6f7ad6a491ffd14b9bd62c156dfa6d8f12ea9b4
afyqzarof <norafyqe@gmail.com>
2025-10-28 12:12:24 +0800

    feat: update custom macros
    print visual item according to language

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,21 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- CUSTOM MACORS -
+-- CUSTOM MACROS -
 local esc = vim.api.nvim_replace_termcodes("<Esc>", true, true, true)
-vim.fn.setreg("l", "yoconsole.log('" .. esc .. "pa:" .. esc .. "la," .. esc .. "pl")
+
+-- Python print (detects automatically)
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "python" },
+  callback = function()
+    vim.fn.setreg("l", "yoprint(f'" .. esc .. "pa: {" .. esc .. "pa}'" .. esc .. "l)")
+  end,
+})
+
+-- JavaScript/TypeScript (explicit setting for these filetypes)
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
+  callback = function()
+    vim.fn.setreg("l", "yoconsole.log('" .. esc .. "pa:" .. esc .. "la," .. esc .. "pl")
+  end,
+})
